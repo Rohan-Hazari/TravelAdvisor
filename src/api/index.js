1e8d3cade89b98efc5ea8cf53d09901e7fccd555
Rohan Hazari <rohanhazari2012@gmail.com>
2023-02-13 23:21:00 +0530

    Added all functionalities API issue remaining

---
+++ src/api/index.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const url =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(url, {
+      method: "GET",
+      url: `https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary`,
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        "X-RapidAPI-Key": JSON.stringify(import.meta.env),
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
